2678ce58b8ffbc02daad8ffefcd38f2b51c6b975
oorellana95 <oorellana30.95@gmail.com>
2023-04-25 08:29:23 -0300

    Deploy Gaia Aleph to GitHub Pages

---
+++ precache-manifest.22bde06c9913464b142e36025fe5fcc7.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf7821789264532c77666b59bac0104",
+    "url": "/gaia-aleph/index.html"
+  },
+  {
+    "revision": "73dd38801f7cdf264040",
+    "url": "/gaia-aleph/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "79fd156ead451ed87a21",
+    "url": "/gaia-aleph/static/css/main.c032d87c.chunk.css"
+  },
+  {
+    "revision": "73dd38801f7cdf264040",
+    "url": "/gaia-aleph/static/js/2.377e789a.chunk.js"
+  },
+  {
+    "revision": "4557c15fd8984004468e8d016ab0ee8b",
+    "url": "/gaia-aleph/static/js/2.377e789a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fd156ead451ed87a21",
+    "url": "/gaia-aleph/static/js/main.0c388c6d.chunk.js"
+  },
+  {
+    "revision": "1abfa979182062f29f76",
+    "url": "/gaia-aleph/static/js/runtime-main.3b50deec.js"
+  },
+  {
+    "revision": "3abaf9c0303914421ac06adc034643e8",
+    "url": "/gaia-aleph/static/media/1.3abaf9c0.jpg"
+  },
+  {
+    "revision": "c21d7224413625a8285dc7c8f05cc1f8",
+    "url": "/gaia-aleph/static/media/10.c21d7224.jpg"
+  },
+  {
+    "revision": "97331a8b676d38a5bfd19bb921db7a34",
+    "url": "/gaia-aleph/static/media/11.97331a8b.jpg"
+  },
+  {
+    "revision": "2fd7c0ef5384b9d1d7251779ea9c298c",
+    "url": "/gaia-aleph/static/media/12.2fd7c0ef.jpg"
+  },
+  {
+    "revision": "385f6944f0ea8c17665a41d00578789e",
+    "url": "/gaia-aleph/static/media/13.385f6944.jpg"
+  },
+  {
+    "revision": "1616792f5589c3db755d5f87b9a50c56",
+    "url": "/gaia-aleph/static/media/14.1.1616792f.jpg"
+  },
+  {
+    "revision": "99ed638171ae0564e2c42d738d82f05c",
+    "url": "/gaia-aleph/static/media/14.99ed6381.jpg"
+  },
+  {
+    "revision": "e3b97cd2f78e8ce5a278617753fe864d",
+    "url": "/gaia-aleph/static/media/15.e3b97cd2.jpg"
+  },
+  {
+    "revision": "619f9d6e5235b2245fbd2ef82136914a",
+    "url": "/gaia-aleph/static/media/16.619f9d6e.jpg"
+  },
+  {
+    "revision": "f3e4db9bcd17aee8a337b869fdd39f18",
+    "url": "/gaia-aleph/static/media/17.f3e4db9b.jpg"
+  },
+  {
+    "revision": "047e1c359d70ae6eed7e0dba3e884650",
+    "url": "/gaia-aleph/static/media/18.047e1c35.jpg"
+  },
+  {
+    "revision": "4bdfb04a27eb1ed11f636aa0a8f05ec0",
+    "url": "/gaia-aleph/static/media/19.4bdfb04a.jpg"
+  },
+  {
+    "revision": "554c3fb8849e90d3d7de76fe1107be37",
+    "url": "/gaia-aleph/static/media/2.554c3fb8.jpg"
+  },
+  {
+    "revision": "e798b3a54778d1d5d077e6b1a5ffa811",
+    "url": "/gaia-aleph/static/media/3.2.e798b3a5.jpg"
+  },
+  {
+    "revision": "5dba9e7faf6e1f7c63a978a7e893c0d2",
+    "url": "/gaia-aleph/static/media/3.5dba9e7f.jpg"
+  },
+  {
+    "revision": "d1048b56bc27ab5511872c8ce5040a57",
+    "url": "/gaia-aleph/static/media/4.d1048b56.jpg"
+  },
+  {
+    "revision": "57ea340d69f4e634b2aaafe494c8e12c",
+    "url": "/gaia-aleph/static/media/5.57ea340d.jpg"
+  },
+  {
+    "revision": "8d0dbad97abd0deb2490e61a3c2557a9",
+    "url": "/gaia-aleph/static/media/6.8d0dbad9.jpg"
+  },
+  {
+    "revision": "53f71e16379feef7a979adc22c8b4d44",
+    "url": "/gaia-aleph/static/media/7.53f71e16.jpg"
+  },
+  {
+    "revision": "d8fa6a7dc31658ffdc3dab3c8bd64377",
+    "url": "/gaia-aleph/static/media/8.d8fa6a7d.jpg"
+  },
+  {
+    "revision": "686c4326d2e2e3b39210c08352c74b25",
+    "url": "/gaia-aleph/static/media/9.686c4326.jpg"
+  },
+  {
+    "revision": "85362e6d651d835063a7e10140878099",
+    "url": "/gaia-aleph/static/media/about-header.85362e6d.jpg"
+  },
+  {
+    "revision": "99846f2c2f87ea34ab2a13fd9805ff9e",
+    "url": "/gaia-aleph/static/media/carousel1.99846f2c.jpg"
+  },
+  {
+    "revision": "ac6c6fe14ca78abf9a92764257f68a8e",
+    "url": "/gaia-aleph/static/media/carousel2.ac6c6fe1.jpg"
+  },
+  {
+    "revision": "1a3651a663a5f2b11ca1596494fd613d",
+    "url": "/gaia-aleph/static/media/carousel3.1a3651a6.jpg"
+  },
+  {
+    "revision": "50e0f2b00f469f1646f08555a224c784",
+    "url": "/gaia-aleph/static/media/check.50e0f2b0.png"
+  },
+  {
+    "revision": "7aa1511d3ca9298fcb52c5235876decf",
+    "url": "/gaia-aleph/static/media/contact-header.7aa1511d.jpg"
+  },
+  {
+    "revision": "ee5eaee43a62c974342a21d3e0c9e939",
+    "url": "/gaia-aleph/static/media/cow-mask0.ee5eaee4.jpg"
+  },
+  {
+    "revision": "8a6fd777cb3541255bad4801f575c76c",
+    "url": "/gaia-aleph/static/media/cow-mask1.8a6fd777.png"
+  },
+  {
+    "revision": "2097f0b4c76b34e160630583c739de49",
+    "url": "/gaia-aleph/static/media/cow-mask2.2097f0b4.png"
+  },
+  {
+    "revision": "246045a87c864931416d64d511ffda11",
+    "url": "/gaia-aleph/static/media/devs-team.246045a8.png"
+  },
+  {
+    "revision": "3af27868d0e384597841942daf71cd87",
+    "url": "/gaia-aleph/static/media/email-pikachu.3af27868.jpg"
+  },
+  {
+    "revision": "c981e595d426ae51cab0fdd8c129f1ca",
+    "url": "/gaia-aleph/static/media/farmers-header.c981e595.png"
+  },
+  {
+    "revision": "ea8bdaf13bd3603d554299d1081e2abf",
+    "url": "/gaia-aleph/static/media/fgv-building.ea8bdaf1.jpg"
+  },
+  {
+    "revision": "c0e9e226adb3da98be0dc79844628ae4",
+    "url": "/gaia-aleph/static/media/fgv-logo.c0e9e226.jpg"
+  },
+  {
+    "revision": "14fcbbcddfc8c780b8a22c1366a689e6",
+    "url": "/gaia-aleph/static/media/flipped_hitos.14fcbbcd.mp4"
+  },
+  {
+    "revision": "f7c876ff06c7190be0cd5e72a5b24907",
+    "url": "/gaia-aleph/static/media/flipped_lara.f7c876ff.mp4"
+  },
+  {
+    "revision": "8c30ad146ab4e301c4b6ee07a1dfabfa",
+    "url": "/gaia-aleph/static/media/flipped_orellana.8c30ad14.mp4"
+  },
+  {
+    "revision": "ae37e3befa1d23df12b9f240bebc846c",
+    "url": "/gaia-aleph/static/media/gaia-staff.ae37e3be.jpg"
+  },
+  {
+    "revision": "a41cbd3667728775346e09fc05fd2cf0",
+    "url": "/gaia-aleph/static/media/home-header.a41cbd36.png"
+  },
+  {
+    "revision": "cb65e3f19982c65a3e4c1e2c57b2793b",
+    "url": "/gaia-aleph/static/media/interreto.cb65e3f1.png"
+  },
+  {
+    "revision": "702e3055b5e69c1773c41659e7bb7c53",
+    "url": "/gaia-aleph/static/media/logo-gaia.702e3055.png"
+  },
+  {
+    "revision": "fe3d38157131470aea40d478f3eb4149",
+    "url": "/gaia-aleph/static/media/logo.fe3d3815.svg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/gaia-aleph/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/gaia-aleph/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/gaia-aleph/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/gaia-aleph/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "1144a5ff99a00fcd1fa866277c07d923",
+    "url": "/gaia-aleph/static/media/oorellana.1144a5ff.jpg"
+  },
+  {
+    "revision": "74c35d32f4a806d3e1bfbc6441c09504",
+    "url": "/gaia-aleph/static/media/pikachu_sound.74c35d32.mp3"
+  },
+  {
+    "revision": "0d6632dbcb3071c7c5e4b0c0ec91b1b1",
+    "url": "/gaia-aleph/static/media/platform-visibility1.0d6632db.png"
+  },
+  {
+    "revision": "369df68b7ebf1e8061e958c498351639",
+    "url": "/gaia-aleph/static/media/platform-visibility2.369df68b.png"
+  },
+  {
+    "revision": "32a7541af03d3134ec9bbe7c165fa22d",
+    "url": "/gaia-aleph/static/media/platform-visibility3.32a7541a.png"
+  },
+  {
+    "revision": "f70b6db5205a23eaa40b8a4b05808fd4",
+    "url": "/gaia-aleph/static/media/portrait-video-home.f70b6db5.png"
+  },
+  {
+    "revision": "594bf57dea7ee8e1e00f60678b889c73",
+    "url": "/gaia-aleph/static/media/portrait-video.594bf57d.png"
+  },
+  {
+    "revision": "3e2dafd5f19ebf67a1bb0f7cd3e9186c",
+    "url": "/gaia-aleph/static/media/raphael-staff.3e2dafd5.jpg"
+  },
+  {
+    "revision": "29f41c75d138a39e3c0f14a5ca1010ba",
+    "url": "/gaia-aleph/static/media/video.29f41c75.png"
+  }
+]);
